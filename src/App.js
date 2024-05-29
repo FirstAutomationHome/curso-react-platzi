f07a10ab3abf86842c4877c59335aa695660389f
fautomationhome <fautomationhome@gmail.com>
2024-05-29 08:14:54 -0500

    Orginazing floders and app.js

--- src/App.js
+++ src/App.js
@@ -1,99 +1,64 @@
 import React from 'react';
-import { TodoCounter } from './components/TodoCounter';
-import { TodoSearch } from './components/TodoSearch';
-import { TodoList } from './components/TodoList';
-import TodoItem from './components/TodoItem';
-import { CreateTodoButton } from './components/CreateTodoButton';
-import { useLocalStorage } from './hooks/useLocalStorage'
-import './App.css';
+import { AppUI } from './logic/AppUI';
+import { useLocalStorage } from './hooks/useLocalStorage';
 
 // const defaultTodos = [
-//   { text: 'Cortar Cebolla', completed: true },
-//   { text: 'Tomar el curso Introducción JS', completed: false },
-//   { text: 'Llorar con la llorona', completed: false },
-//   { text: 'LALALALAL', completed: false },
-//   { text: 'usar estados de React', completed: true },
-// ]
+//   { text: 'Cortar cebolla', completed: true },
+//   { text: 'Tomar el Curso de Intro a React.js', completed: false },
+//   { text: 'Llorar con la Llorona', completed: false },
+//   { text: 'LALALALALA', completed: false },
+//   { text: 'Usar estados derivados', completed: true },
+// ];
 
 // localStorage.setItem('TODOS_V1', JSON.stringify(defaultTodos));
 // localStorage.removeItem('TODOS_V1');
 
-
-
 function App() {
+  const [todos, saveTodos] = useLocalStorage('TODOS_V1', []);
+  const [searchValue, setSearchValue] = React.useState('');
 
-  const [todos, saveTodos] = useLocalStorage('TODOS_V1',[])
-
-  //Estado para la lectura del input o buscador
-  const [searchValue, setSearchValue] = React.useState('')
-  // console.log('Los usuarios buscan todos de: ' + searchValue)
-
-  //Cantidad de TODOs completados
   const completedTodos = todos.filter(
-    todo => !!todo.completed).length
-  
-  //Total de todos
-  const totalTodos = todos.length
+    todo => !!todo.completed
+  ).length;
+  const totalTodos = todos.length;
 
-  //Buscador de TODOs
   const searchedTodos = todos.filter(
     (todo) => {
-      //con el to LowerCase convierte todo a minúscula
-      const todoText = todo.text.toLowerCase()
-      const searchText = searchValue.toLowerCase()
-      //
-      return todoText.includes(searchText)
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);
     }
-  )
+  );
 
-  
-
-  // Función para marcar los TODOs completados al dar click en el check
   const completeTodo = (text) => {
-    const newTodos = [...todos]
+    const newTodos = [...todos];
     const todoIndex = newTodos.findIndex(
       (todo) => todo.text === text
-    )
-    newTodos[todoIndex].completed = true
-    saveTodos(newTodos)
-  }
+    );
+    newTodos[todoIndex].completed = true;
+    saveTodos(newTodos);
+  };
 
-  // Función para eliminar los TODOs al dar click en la X
   const deleteTodo = (text) => {
-    const newTodos = [...todos]
+    const newTodos = [...todos];
     const todoIndex = newTodos.findIndex(
       (todo) => todo.text === text
-    )
-    newTodos.splice(todoIndex, 1)
-    saveTodos(newTodos)
-  }
-
-
+    );
+    newTodos.splice(todoIndex, 1);
+    saveTodos(newTodos);
+  };
+  
   return (
-    <>
-      <TodoCounter 
-        completed={completedTodos}
-        total={totalTodos} />
-      <TodoSearch 
-        searchValue={searchValue}
-        setSearchValue={setSearchValue} />
-
-      <TodoList>
-        {searchedTodos.map(todo => (
-          <TodoItem 
-            key={todo.text}
-            text={todo.text} 
-            completed={todo.completed}
-            onComplete={ () => completeTodo(todo.text)}
-            onDelete={ () => deleteTodo(todo.text)}/>
-        ))}
-      </TodoList>
-
-      <CreateTodoButton />
-      
-    </>
+    <AppUI
+      completedTodos={completedTodos}
+      totalTodos={totalTodos}
+      searchValue={searchValue}
+      setSearchValue={setSearchValue}
+      searchedTodos={searchedTodos}
+      completeTodo={completeTodo}
+      deleteTodo={deleteTodo}
+    />
   );
 }
-
 
 export default App;
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -20,3 +20,4 @@ export default function TodoItem({ text, completed, onComplete, onDelete }) {
     
   )
 }
+export {TodoItem}
